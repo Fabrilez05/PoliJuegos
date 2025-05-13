6572edc6aa2c98e5b39659a64d0240362d474af2
Yoprolezpy05 <94013174+Fabrilez05@users.noreply.github.com>
2025-05-13 11:29:22 -0300

    Add files via upload

---
+++ logicaJuego.py
@@ -0,0 +1,134 @@
+import random
+
+
+ABECEDARIO:list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+FRECUENCIA_LETRAS: list = {
+    'a': 12.53, 'b': 1.42, 'c': 4.68, 'd': 5.86, 'e': 13.68,
+    'f': 0.69, 'g': 1.01, 'h': 0.70, 'i': 6.25, 'j': 0.44,
+    'k': 0.02, 'l': 4.97, 'm': 3.15, 'n': 6.71, 'ñ': 0.31, 
+    'o': 8.68, 'p': 2.51, 'q': 0.88, 'r': 6.87, 's': 7.98,
+    't': 4.63, 'u': 3.93, 'v': 0.90, 'w': 0.01, 'x': 0.22,
+    'y': 0.90, 'z': 0.52
+}
+
+class Juego:
+    def __init__(self):
+        self.palabras: list = []
+        self.letrasJugables: list = []
+        self.diccionario = 'diccionario.txt'
+        self.letraGenerada: str = ''
+    
+    def generarLetra(self) -> str:
+        letrasPeso = []
+        for letra, peso in FRECUENCIA_LETRAS.items():
+            letrasPeso.extend([letra] * int(peso * 100))
+        return random.choice(letrasPeso)
+    
+    def generarLetrasJugables(self) -> None:
+        self.letrasJugables.clear()
+        self.letrasJugables.append(self.letraGenerada)
+        letrasSobrantes = [letra for letra in ABECEDARIO if letra != self.letraGenerada]
+        letrasSobrantesPeso = []
+        for letra in letrasSobrantes:
+            letrasSobrantesPeso.extend([letra] * int(FRECUENCIA_LETRAS[letra] * 100))
+        
+        while len(self.letrasJugables) < 7:
+            nuevaLetra = random.choice(letrasSobrantesPeso)
+            if nuevaLetra not in self.letrasJugables:
+                self.letrasJugables.append(nuevaLetra)
+        
+        random.shuffle(self.letrasJugables)
+
+    def obtenerPalabras(self) -> None:
+        self.palabras.clear()
+        with open(self.diccionario, 'r', encoding="utf-8") as file:
+            todasPalabras = [palabra.strip().lower() for palabra in file.readlines()]
+            random.shuffle(todasPalabras)
+            
+            for palabra in todasPalabras:
+                if (self.letraGenerada in palabra and 
+                    all(letra in self.letrasJugables for letra in palabra)):
+                    self.palabras.append(palabra)
+                    if len(self.palabras) >= 100:
+                        break
+    
+    def disminuirPalabras(self) -> None:
+        if len(self.palabras) > 100:
+            self.palabras = random.sample(self.palabras, 100)
+
+
+
+class pilaEntrada:
+    def __init__(self):
+        self.pila = []
+
+    def push(self, letra):
+        self.pila.append(letra)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.pila.pop()
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.pila) == 0
+
+    def size(self):
+        return len(self.pila)
+    
+    def empty(self):
+        self.pila.clear()
+
+
+
+'''
+def main() -> None:
+    juego = Juego()
+    juego.letraGenerada = juego.generarLetra()
+    juego.generarLetrasJugables()
+    juego.obtenerPalabras()
+    juego.disminuirPalabras()
+
+
+
+    pilaDeEntrada = pilaEntrada()
+
+
+
+
+    while True:
+
+        print(juego.palabras)
+
+        print(f"ingrese una letra de : {juego.letrasJugables}, la palabra debe contener la letra generada: {juego.letraGenerada}")
+        entrada: str = input()
+
+        while entrada not in juego.letrasJugables or len(entrada) > 1:
+            print(f"ingrese una letra de : {juego.letrasJugables}, la palabra debe contener la letra generada: {juego.letraGenerada}")
+            entrada = input()
+
+        pilaDeEntrada.push(entrada)
+
+        print ("desea evaluar la palabra? (si/no)")
+        evaluar: str = input()
+
+        if evaluar.lower() == "si":
+            entrada = "".join(pilaEntrada.pila)
+            while not pilaEntrada.is_empty():
+                if pilaDeEntrada.size() < 3:
+                    print("la palabra es muy corta")
+                    pilaEntrada.empty()
+                elif juego.letraGenerada not in pilaEntrada.pila:
+                    print("la palabra no contiene la letra generada")
+                    pilaDeEntrada.empty()
+                elif entrada not in juego.palabras:
+                    print("la palabra no existe")
+                    pilaDeEntrada.empty()
+                else:
+                    print("la palabra es correcta")
+                    pilaDeEntrada.empty()
+
+'''
+
